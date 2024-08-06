cc5fabe6ce71a3625212169d93c28d79c80d7c1e
danimsteger <daniellestegertutoring@gmail.com>
2024-08-06 17:45:15 -0400

    setup initial folders and began model

---
+++ models/User.js
@@ -0,0 +1,15 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
