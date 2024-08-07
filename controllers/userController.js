1b81de5e6f19c2fb2c24cac25370b1c16f7ac455
danimsteger <daniellestegertutoring@gmail.com>
2024-08-07 19:08:32 -0400

    add friend to user route

--- controllers/userController.js
+++ controllers/userController.js
@@ -73,4 +73,23 @@ module.exports = {
       return res.status(500).json(err);
     }
   },
+
+  async addFriend(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $addToSet: { friends: req.params.friendId } },
+        { runValidators: true, new: true }
+      );
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user found with that ID!' });
+      }
+
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,10 +5,13 @@ const {
   getOneUser,
   updateUser,
   deleteUser,
+  addFriend,
 } = require('../../controllers/userController');
 
 router.route('/').get(getUsers).post(createUser);
 
 router.route('/:userId').get(getOneUser).put(updateUser).delete(deleteUser);
+
+router.route('/:userId/friends/:friendId').post(addFriend);
 
 module.exports = router;
