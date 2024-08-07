a450eacb000692039fafd176558e6c18d10ab373
danimsteger <daniellestegertutoring@gmail.com>
2024-08-07 18:53:05 -0400

    update user route

--- controllers/userController.js
+++ controllers/userController.js
@@ -39,14 +39,24 @@ module.exports = {
     }
   },
 
-  // async updateUser(req, res) {
-  //   try {
+  async updateUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
 
-  //   } catch (err) {
-  //     console.log(err);
-  //     return res.status(500).json(err);
-  //   }
-  // }
+      if (!user) {
+        return res.status(404).json({ message: 'No user with that ID!' });
+      }
+
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
 
   // async deleteUser(req, res) {
   //   try {
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,9 +3,11 @@ const {
   getUsers,
   createUser,
   getOneUser,
+  updateUser,
 } = require('../../controllers/userController');
 
 router.route('/').get(getUsers).post(createUser);
 
-router.route('/:userId').get(getOneUser);
+router.route('/:userId').get(getOneUser).put(updateUser);
+
 module.exports = router;
