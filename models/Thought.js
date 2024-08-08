8737e180f905673c5565c7288e4c44f393dec65a
danimsteger <daniellestegertutoring@gmail.com>
2024-08-07 22:27:26 -0400

    format date getter

--- models/Thought.js
+++ models/Thought.js
@@ -6,12 +6,13 @@ const thoughtSchema = new Schema(
     thoughtText: {
       type: String,
       required: true,
-      minLength: [1, 'Not long enough!'],
+      minLength: [1],
       maxLength: [280, 'Thoughts cannot exceed 280 characters'],
     },
     createdAt: {
       type: Date,
-      default: Date.now(),
+      default: Date.now,
+      get: formatDate,
     },
     username: {
       type: String,
@@ -22,6 +23,7 @@ const thoughtSchema = new Schema(
   {
     toJSON: {
       virtuals: true,
+      getters: true,
     },
     id: false,
   }
@@ -30,6 +32,29 @@ const thoughtSchema = new Schema(
 thoughtSchema.virtual('reactionCount').get(function () {
   return this.reactions.length;
 });
+
+function formatDate(createdAt) {
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  const day = createdAt.getDate();
+  const month = months[createdAt.getMonth()];
+  const year = createdAt.getFullYear();
+  const time = createdAt.toLocaleTimeString();
+
+  return month + ' ' + day + ', ' + year + ' at ' + time;
+}
 
 const Thought = model('thought', thoughtSchema);
 
